958cd9cbd9a64f06a2cdb46ee14c702bec10259a
Rens Dur <r.c.m.dur@student.tudelft.nl>
2022-04-29 17:13:50 +0200

    Implement Level Curve functionality
    Implement two classes: LevelCurve and LevelCurveMap, which wraps multiple level curves into one construct and provides functionality to work with the combination as a whole.

--- 3D_model_construction_rust/src/classes/point.rs
+++ 3D_model_construction_rust/src/classes/point.rs
@@ -1,3 +1,4 @@
+#[derive (Debug)]
 pub struct Point {
     pub x: f64,
     pub y: f64,
--- 3D_model_construction_rust/src/main.rs
+++ 3D_model_construction_rust/src/main.rs
@@ -1,11 +1,10 @@
 mod classes;
-use classes::point::*;
+use classes::point::Point;
+use classes::raster::Raster;
+use classes::level_curves::{LevelCurve, LevelCurveMap};
 
 fn main() {
     
-    let mut p = Point{ x: 10.0, y: 10.0, z: 10.0 };
-    let mut p2 = Point{ x:20.0, y: 10.0, z: 10.0 };
-
-    println!("{}", Point::dist(&p, &p2));
+    
 
 }
