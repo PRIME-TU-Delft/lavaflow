48f4724febcb3aab05a36fc11b38a912215bdb92
Pauline <6712pauline@gmail.com>
2022-04-29 17:44:42 +0200

    Start implementation construction algorithm

--- 3D_model_construction_rust/src/classes/cardinalIntersection.rs
+++ 3D_model_construction_rust/src/classes/cardinalIntersection.rs
@@ -8,7 +8,7 @@ use super::raster::*;
  //  TODO; rest of directions
 pub fn n(ix: u64, iy: u64, raster: Raster, isSVC: &Vec<Vec<bool>> ) -> (f64, u64){
         for y in iy..raster.rows {
-            if isSVC[(ix as usize)][(y as usize)] == true {
+            if isSVC[ix as usize][y as usize] == true {
                 return (raster.altitudes[ix as usize][y as usize], iy - y );
             }
         }
--- 3D_model_construction_rust/src/classes/constructor.rs
+++ 3D_model_construction_rust/src/classes/constructor.rs
@@ -1,8 +1,7 @@
 use super::raster::Raster;
 
 #[derive(Debug)]
-pub struct Constructor {
-    rasterSize : u64,
+pub struct ModelConstructor {
     contourMargin: u64,
 
     isSVC : Vec<Vec<bool>>,
@@ -12,14 +11,28 @@ pub struct Constructor {
 
 trait helperFunctions{
     fn initialize();
-    fn checkSVC(p:  Vec<f64> , ix: i64, iy: i64 ) -> bool;
+    fn checkSVC(p:  Vec<f64> , ix: i64, iy: i64 ) -> bool; 
+    // fn getClostestCOntourPoint(p, Vec<f64>, );
     fn localTIN(p: Vec<f64>) ;
     fn calcHeightsNVCs() ;
 } 
 pub trait ConstructorFunct{
     //TODO; add levelCurveMap to parameter list
-    fn constructMap(rastersize: i64, contourMargin: i64);
+    fn constructMap(&self, raster: Raster, contourMargin: i64) -> &Raster;
 } 
+
+impl  ModelConstructor {
+
+    fn constructMap(&self, raster: &Raster, contourMargin: i64) -> &Raster{ 
+
+        let x = raster.columns;
+        let y = raster.rows;
+        self.isSVC = [[false; (x as *const u64) ] ; y].to_vec();
+        return raster;
+    }
+
+
+}
 
 
 
