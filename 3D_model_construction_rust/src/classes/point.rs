31df9ade04bef55e539699e1b8f088193df77d8a
Rens Dur <r.c.m.dur@student.tudelft.nl>
2022-04-29 14:15:43 +0200

    Implement the Point class and create module structure
    The module structure is created and a Point class has been implemented.

---
+++ 3D_model_construction_rust/src/classes/point.rs
@@ -0,0 +1,18 @@
+pub struct Point {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64
+}
+
+impl Point {
+    // Constructor is not necessary
+    
+    pub fn distSqr(a: &Point, b: &Point) -> f64 {
+        return (b.x - a.x).powi(2) + (b.y - a.y).powi(2) + (b.z - a.z).powi(2);
+    }
+
+    pub fn dist(a: &Point, b: &Point) -> f64 {
+        return f64::sqrt(Point::distSqr(a, b));
+    }
+
+}
--- 3D_model_construction_rust/src/main.rs
+++ 3D_model_construction_rust/src/main.rs
@@ -1,3 +1,11 @@
+mod classes;
+use classes::point::*;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let mut p = Point{ x: 10.0, y: 10.0, z: 10.0 };
+    let mut p2 = Point{ x:20.0, y: 10.0, z: 10.0 };
+
+    println!("{}", Point::dist(&p, &p2));
+
 }
