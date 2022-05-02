affc8260629f482233b76d7dc59931b6c7d03673
Rens Dur <r.c.m.dur@student.tudelft.nl>
2022-05-02 13:47:19 +0200

    Implement setRasterEdgesToZero in the Modelconstructor
    The setRasterEdgesToZero function is used to make sure the edges of the mountain have level zero.

--- 3D_model_construction_rust/src/classes/constructor.rs
+++ 3D_model_construction_rust/src/classes/constructor.rs
@@ -1,12 +1,12 @@
-use super::raster::Raster;
+use super::{raster::Raster, level_curves::{LevelCurveMap, LevelCurve}};
 
 #[derive(Debug)]
 pub struct ModelConstructor {
 	contour_margin: u64,
+    level_curve_map: LevelCurveMap,
 	is_svc: Vec<Vec<bool>>,
 	raster: Raster
 }
-
 
 // @Pauline are you planning to implement this trait for other structs as well?
 // Otherwise using a trait is unnecessary, you can just implement functions directly for a struct without using traits.
@@ -20,10 +20,34 @@ trait HelperFunctions {
 	fn calc_heights_nvcs();
 }
 
+
+// Implementation of the ModelConstructor
 impl ModelConstructor {
+
+    // Function: Initialize the SVC map with 'false' for every element
 	fn construct_map(&mut self, raster: &Raster, _contour_margin: i64) {
-		let x = raster.columns;
-		let y = raster.rows;
-		self.is_svc = vec![vec![false; x]; y];
+		
 	}
+
+    // Function: initialize
+    fn setRasterEdgesToZero(&mut self) {
+
+        // Initialize all the outer-most raster values to zero
+        // (According to requirement that the edges of the mountain must have level 0)
+        for i in 0..self.raster.rows {
+            for j in 0..self.raster.columns {
+                // If this box is on the outer edge
+                if i == 0 || j == 0 || i == self.raster.rows-1 || j == self.raster.columns-1 {
+                    // Set its value to zero
+                    self.raster.set(i, j, 0.0);
+                    
+                    // Set this box to an svc-box
+                    self.is_svc[i][j] = true;
+                }
+            }
+        }
+
+    }
+
 }
+
