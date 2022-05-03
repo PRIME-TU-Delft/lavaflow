fb602e97267586978920d558a87d4fafaf95e60e
abel <abeldebruijn@hotmail.com>
2022-05-03 09:48:09 +0200

    Setup sveltekit

---
+++ svelte-app/svelte.config.js
@@ -0,0 +1,19 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
+	}
+};
+
+export default config;
