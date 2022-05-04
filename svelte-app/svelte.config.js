70db5b26bdb226ff1f399a443d27c8bafce41578
abel <a.debruijn-3@student.tudelft.nl>
2022-05-04 13:05:11 +0200

    Added: scss

--- svelte-app/svelte.config.js
+++ svelte-app/svelte.config.js
@@ -5,14 +5,30 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+			scss: {
+				prependData: '@use "src/variables.scss" as *;'
+			}
+		})
+	],
 
 	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: null
-		})
+		}),
+
+		vite: {
+			css: {
+				preprocessorOptions: {
+					scss: {
+						additionalData: '@use "src/variables.scss" as *;'
+					}
+				}
+			}
+		}
 	}
 };
 
