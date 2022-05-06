a35c81d741d52b70ad89b8fdebfdbc3ac24b7438
abel <a.debruijn-3@student.tudelft.nl>
2022-05-06 13:51:08 +0200

    Added: instruction

--- svelte-app/src/lib/components/Instructions.svelte
+++ svelte-app/src/lib/components/Instructions.svelte
@@ -1,34 +1,12 @@
 <script lang="ts">
-	interface Instruction {
-		title: string;
-		description: string;
-	}
-
-	let instructions: Instruction[] = [
-		{
-			title: '3 markers visible',
-			description: 'To recognize the level curves, we need to have 3 markers visible'
-		},
-		{
-			title: '3 markers visible',
-			description: 'To recognize the level curves, we need to have 3 markers visible'
-		},
-		{
-			title: '3 markers visible',
-			description: 'To recognize the level curves, we need to have 3 markers visible'
-		},
-		{
-			title: '3 markers visible',
-			description: 'To recognize the level curves, we need to have 3 markers visible'
-		},
-		{
-			title: '3 markers visible',
-			description: 'To recognize the level curves, we need to have 3 markers visible'
-		}
-	];
+	import { instructions } from '$lib/data/instructions';
 </script>
 
 <div class="instructions">
+	<h2>Good example of contour map</h2>
+	<div class="example" />
+
+	<h2>Instructions</h2>
 	{#each instructions as instruction}
 		<div class="instruction">
 			<div class="title">{instruction.title}</div>
@@ -38,17 +16,25 @@
 </div>
 
 <style lang="scss">
+	.example {
+		height: 15rem;
+		background: url('/example.png') no-repeat center center;
+		background-size: contain;
+	}
+
 	.instructions {
 		padding: 3rem 1rem 1rem;
-		color: vat(--text-color);
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
+		color: var(--text-color);
 		height: 100%;
 		overflow: auto;
+		max-width: 30rem;
+		margin: 0 auto;
 
-		.instruction .title {
-			font-size: 1.2rem;
+		.instruction {
+			margin: 0.5rem auto;
+			.title {
+				font-size: 1.2rem;
+			}
 		}
 	}
 </style>
