bd24987400d7cf9e0050fe361963eee69e56a664
Pauline <6712pauline@gmail.com>
2022-05-06 16:45:45 +0200

    Generate triangles from vertices

--- 3D_model_construction_rust/src/lib.rs
+++ 3D_model_construction_rust/src/lib.rs
@@ -29,9 +29,35 @@ pub fn generate_3d_model<'a>( tree : &'a mut LevelCurveTree<'a>,  contour_margin
 
         //create new modelConstructor (module containing 3D-model construction algorithm)
         let mut model_constructor = ModelConstructor::new(&mut raster, contour_margin, &level_curve_map);
-        //get heights in raster
+
+        //determine heights
         model_constructor.construct();
-        let heights = raster.altitudes;
+
+        //get heights from raster
+        let heights = &raster.altitudes;
+
+        //convert height raster to flat list of x,y,z points for GLTF format
+        //every cell had 4 corners, becomes two triangles
+        let mut     final_points: Vec<(f64, f64, f64)> = Vec::new();
+        for x in 0..raster.rows {
+            for y in 0 ..raster.columns {
+                //triangle 1
+                //(0,0)
+                final_points.push((columns  as f64 * raster.column_width, (rows - y) as f64 * raster.row_height, heights[x][y].unwrap()));
+                //(0, 1)
+                final_points.push((columns as f64 * raster.column_width, ((rows - y) as f64 + 1.0) * raster.row_height, heights[x][y + 1].unwrap()));
+                //(1, 1)
+                final_points.push(((columns  as f64 + 1.0) * raster.column_width, ((rows - y) as f64 + 1.0) * raster.row_height, heights[x + 1][y + 1].unwrap()));
+
+                //triangle 2
+                //(0, 0)
+                final_points.push((columns as f64 * raster.column_width, (rows - y) as f64 * raster.row_height, heights[x][y].unwrap()));
+                //(1, 0)
+                final_points.push(((columns as f64  + 1.0) * raster.column_width, (rows - y) as f64 * raster.row_height, heights[x + 1][y].unwrap()));
+                //(1, 1)
+                final_points.push(((columns as f64 + 1.0) * raster.column_width, ((rows - y) as f64 + 1.0) * raster.row_height, heights[x][y+ 1].unwrap()));
+            }
+        }
 
         //TODO: add conversion to GLTF
          return Vec::new();
