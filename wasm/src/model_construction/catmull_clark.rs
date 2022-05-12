b486038cbd62803b688da1faa10861e98f9a0ca4
Pauline <6712pauline@gmail.com>
2022-05-12 11:00:57 +0200

    Finish implementations
    basic implementation done, alot of //TODO's left

--- wasm/src/model_construction/catmull_clark.rs
+++ wasm/src/model_construction/catmull_clark.rs
@@ -1,4 +1,6 @@
-use super::raster::Raster;
+use std::{collections::HashMap, usize};
+
+use super::{point::Point, raster::Raster};
 
 /// a face is a square of references to points
 /// should only ever have 4 points
@@ -52,82 +54,109 @@ fn raster_to_faces(raster: &mut Raster, is_sharp: &Vec<Vec<bool>>) -> (Vec<Face>
 // implemented using : https://rosettacode.org/wiki/Catmull%E2%80%93Clark_subdivision_surface
 //TODO modify implementation such that sharp values are not modified
 //TODO dont copy input lists
-fn catmull_clark(f: &Vec<Face>, v: &Vec<Vertex>) -> Result<(), String> {
+fn catmull_clark(fs: &Vec<Face>, vs: &Vec<Vertex>) -> Result<(Vec<Vertex>, Vec<Face>), String> {
+	//
+	// FINDING ALL NEW POINTS
+	//
+
 	// 1. for each face, a face point is created which is the average of all the points of the face.
 	// each entry in the returned list is a vertex.
-
-	let face_points = get_face_points(v, f);
+	let face_points = get_face_points(vs, fs)?;
 
 	// get list of edges with 1 or 2 adjacent faces
 	// [pointnum_1, pointnum_2, facenum_1, facenum_2, center] or
 	// [pointnum_1, pointnum_2, facenum_1, None, center]
 
-	let edges = get_edges_faces(v, f);
+	let edges = get_edges_faces(vs, fs)?;
 
-	// get edge points, a list of points
-	let edge_points = get_edge_points(v, &edges?, &face_points?);
+	// get edge points, a list of points = (average of face points + edge center)/2
+	let edge_points = get_edge_points(vs, &edges, &face_points)?;
 
 	// per point: the average of the face points of the faces the point belongs to (avg_face_points)
-	let avg_face_points = get_average_face_points(v, f, &face_points?);
+	let avg_face_points = get_average_face_points(vs, fs, &face_points)?;
 
 	//per point:  the average of the centers of edges the point belongs to (avg_mid_edges)
-	let avg_mid_edges = get_average_edges(v, &edges?);
+	let avg_mid_edges = get_average_edges(vs, &edges)?;
 
 	// how many faces a point belongs to
-	let points_faces = get_faces_per_point(v, f);
+	let points_faces = get_faces_per_point(vs, fs)?;
 
 	//find out new locations of exisitng points in mesh
 	//TODO sharp points should not ever move in y direction
-	let new_points = get_new_points(v, &points_faces, &avg_face_points, &avg_mid_edges);
+	let mut new_points = get_new_points(vs, &points_faces, &avg_face_points, &avg_mid_edges)?;
 
-	Ok(())
-	// // add face points to new_points
+	//
+	// ADDING NEW POINTS TO USABLE LISTS
+	//
 
-	// let face_point_nums = [];
+	// add face points to new_points
+	// and keep track of indexes of newly added points
+	let mut face_point_index: Vec<usize> = Vec::new();
 
-	// // point num after next append to new_points
-	// let next_pointnum = len(new_points);
+	//TODO is use of & and * here correct? want to avoid copying vec to get len but use literal len.
+	let mut next_index = new_points.len();
 
-	// for face_point in face_points{
-	//     new_points.append(face_point);
-	//     face_point_nums.append(next_pointnum);
-	//     next_pointnum += 1;
-	// }
+	for face_point in face_points {
+		new_points.push(face_point);
+		face_point_index.push(next_index);
+		next_index += 1;
+	}
 
-	// // add edge points to new_points
+	// add edge points to new_points, using hash so you can find index per edge
+	let mut edge_index_map: HashMap<(usize, usize), usize> = HashMap::with_capacity(edges.len());
 
-	// let edge_point_nums = dict()
+	//TODO how to do index trav properly in rust ;-;
+	let mut edgenum = 0;
+	for edge in edges {
+		let edge_point = edge_points.get(edgenum).ok_or("catmull clark: edge does not exist at index")?.clone();
+		new_points.push(edge_point);
+		edge_index_map.insert((edge.p1, edge.p2), next_index);
+		next_index += 1;
+		edgenum += 1;
+	}
 
-	// for edgenum in range(len(edges_faces)){
-	//     let pointnum_1 = edges_faces[edgenum][0];
-	//     let pointnum_2 = edges_faces[edgenum][1];
-	//     let  edge_point = edge_points[edgenum];
-	//     let new_points.append(edge_point);
-	//     let edge_point_nums[(pointnum_1, pointnum_2)] = next_pointnum;
-	//     let next_pointnum += 1;
+	//
+	//  GENERATE FACES USING NEW POINTS
+	//
 
-	//     let new_faces =[];
-	// }
-	// for oldfacenum in range(len(f)){
-	//     let oldface = f[oldfacenum];
-	//     // 4 point face
-	//     if len(oldface) == 4 {
-	//         a = oldface[0];
-	//         b = oldface[1];
-	//         c = oldface[2];
-	//         d = oldface[3];
-	//         face_point_abcd = face_point_nums[oldfacenum]
-	//         edge_point_ab = edge_point_nums[switch_nums((a, b))];
-	//         edge_point_da = edge_point_nums[switch_nums((d, a))];
-	//         edge_point_bc = edge_point_nums[switch_nums((b, c))];
-	//         edge_point_cd = edge_point_nums[switch_nums((c, d))];
-	//         new_faces.append((a, edge_point_ab, face_point_abcd, edge_point_da));
-	//         new_faces.append((b, edge_point_bc, face_point_abcd, edge_point_ab));
-	//         new_faces.append((c, edge_point_cd, face_point_abcd, edge_point_bc));
-	//         new_faces.append((d, edge_point_da, face_point_abcd, edge_point_cd));
-	//     }
-	// }
-	// return new_points, new_faces
+	let mut new_faces: Vec<Face> = Vec::new();
+
+	//TODO how to do index trav properly in rust ;-;
+	let mut facenum = 0;
+
+	for f in fs {
+		// 4 point required in face
+		if f.points.len() == 4 {
+			let a = *f.points.get(0).unwrap();
+			let b = *f.points.get(1).unwrap();
+			let c = *f.points.get(2).unwrap();
+			let d = *f.points.get(3).unwrap();
+			let face_point_abcd = face_point_index[facenum];
+			let edge_point_ab = *edge_index_map.get(&smallest_first(a, b)).ok_or("catmull : egde index ab not found in map")?;
+			let edge_point_da = *edge_index_map.get(&smallest_first(d, a)).ok_or("catmull : egde index ab not found in map")?;
+			let edge_point_bc = *edge_index_map.get(&smallest_first(b, c)).ok_or("catmull : egde index ab not found in map")?;
+			let edge_point_cd = *edge_index_map.get(&smallest_first(c, d)).ok_or("catmull : egde index ab not found in map")?;
+
+			new_faces.push(Face {
+				points: vec![a, edge_point_ab, face_point_abcd, edge_point_da],
+			});
+			new_faces.push(Face {
+				points: vec![b, edge_point_bc, face_point_abcd, edge_point_ab],
+			});
+			new_faces.push(Face {
+				points: vec![c, edge_point_cd, face_point_abcd, edge_point_bc],
+			});
+			new_faces.push(Face {
+				points: vec![d, edge_point_da, face_point_abcd, edge_point_cd],
+			});
+
+			facenum += 1;
+		} else {
+			return Err(String::from("catmull ; face found with less than 4 vertices"));
+		}
+	}
+
+	Ok((new_points, new_faces))
 }
 
 ///
@@ -170,6 +199,14 @@ fn average_of_points(xs: Vec<Vertex>) -> Vertex {
 		y: agr.y / n,
 		z: agr.z / n,
 		is_sharp: false,
+	}
+}
+
+fn smallest_first(p1: usize, p2: usize) -> (usize, usize) {
+	if (p1 <= p2) {
+		(p1, p2)
+	} else {
+		(p2, p2)
 	}
 }
 
@@ -252,8 +289,9 @@ fn get_edges_faces(v: &Vec<Vertex>, f: &Vec<Face>) -> Result<Vec<Edge>, String> 
 		}
 	}
 
+	//TODO : figure out sort in rust
 	// sort edges by pointnum_1, pointnum_2, facenum, for merge step
-	//edges = sorted(edges);
+	//edges =  edges.sort_by(|a, b| a.p1.cmp(a.p2));
 
 	//if two faces share an edge it is currently in edges list twice so
 	// merge edges with 2 adjacent faces
@@ -389,9 +427,10 @@ fn get_faces_per_point(vs: &Vec<Vertex>, fs: &Vec<Face>) -> Result<Vec<usize>, S
 	Ok(faces_per_point)
 }
 
+//TODO SHARP EDGES
 //Move each original point to the new vertex point (F + 2R + (n-3)*v)/n
 //v				//n								//F							//R
-fn get_new_points(vs: &Vec<Vertex>, f_per_v: &Vec<usize>, avg_face_points: &Vec<Vertex>, avg_mid_edges: &Vec<Vertex>) -> Result<(), String> {
+fn get_new_points(vs: &Vec<Vertex>, f_per_v: &Vec<usize>, avg_face_points: &Vec<Vertex>, avg_mid_edges: &Vec<Vertex>) -> Result<(Vec<Vertex>), String> {
 	let mut new_vertices: Vec<Vertex> = Vec::new();
 
 	for i in 0..vs.len() {
@@ -400,12 +439,12 @@ fn get_new_points(vs: &Vec<Vertex>, f_per_v: &Vec<usize>, avg_face_points: &Vec<
 		let F = avg_face_points.get(i).ok_or("get average face points : could not find face")?;
 		let R = avg_mid_edges.get(i).ok_or("get average face points : could not find face")?;
 
-		let x = (v.x * (*n as f32 - 3.0) + (2.0 * R.x) + F.x);
-		let y = (v.y * (*n as f32 - 3.0) + (2.0 * R.y) + F.y);
-		let z = (v.y * (*n as f32 - 3.0) + (2.0 * R.y) + F.y);
+		let x = ((v.x * (*n as f32 - 3.0)) + (2.0 * R.x) + F.x);
+		let y = ((v.y * (*n as f32 - 3.0)) + (2.0 * R.y) + F.y);
+		let z = ((v.y * (*n as f32 - 3.0)) + (2.0 * R.y) + F.y);
 
 		new_vertices.push(Vertex { x, y, z, is_sharp: false })
 	}
 
-	Ok(())
+	Ok(new_vertices)
 }
