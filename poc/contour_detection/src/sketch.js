fbb304ea7a73548bf905fd6fed374e047787ffff
Julia van der Kris <julia@jlia.dev>
2022-05-18 10:18:13 +0200

    Show debugging output for contour detection/deduplication using p5

--- poc/contour_detection/src/sketch.js
+++ poc/contour_detection/src/sketch.js
@@ -1,6 +1,7 @@
-import { detectCurves, drawCurves, getCurves } from "./detectCurves.js";
+import { getCurves } from "./detectCurves.js";
 
 import cv from "opencv-ts";
+import p5 from "p5";
 
 /** @type Draggable[] */
 let points = [];
@@ -9,23 +10,46 @@ let img;
 let opencvDummyImg = document.getElementById("imageSrc");
 let inputElement = document.getElementById("fileInput");
 
-// Trigger: a file has been selected
-inputElement.addEventListener(
-	"change",
-	(e) => {
-		// Create an invisible dummy image for OpenCV to load the file from
-		let imgURL = URL.createObjectURL(e.target.files[0]);
-		opencvDummyImg.src = imgURL;
 
-		// Unhide button
-		document.getElementById("draw-contours-button").style.display = "block";
-	},
-	false
-);
+const sketch = (p5) => {
+	p5.setup = () => {
+		p5.createCanvas(1000, 1000);
+	};
 
-// Trigger: user clicked "draw contours" button
-document.getElementById("draw-contours-button").addEventListener("click", () => {
-	let mat = cv.imread(opencvDummyImg);
 
-	// TODO: use p5 to show debug output
-});
+	// Trigger: a file has been selected
+	inputElement.addEventListener(
+		"change",
+		(e) => {
+			// Create an invisible dummy image for OpenCV to load the file from
+			let imgURL = URL.createObjectURL(e.target.files[0]);
+			opencvDummyImg.src = imgURL;
+
+			// Unhide button
+			document.getElementById("draw-contours-button").style.display = "block";
+		},
+		false
+	);
+
+
+	// Trigger: user clicked "draw contours" button
+	document.getElementById("draw-contours-button").addEventListener("click", () => {
+		let mat = cv.imread(opencvDummyImg);
+
+		let [contours, hierarchy] = getCurves(mat);
+
+		contours.forEach((contour, index) => {
+			// Add text to the contour: "index (index of parent)"
+			p5.text(`${index} (${hierarchy[index]})`, contour[0], contour[1]);
+
+			for (let i = 0; i < contour.length; i += 2) {
+				let x = contour[i];
+				let y = contour[i+1];
+				p5.point(x, y);
+			}
+		});
+	});
+};
+
+
+new p5(sketch, document.getElementById("sketch"));
