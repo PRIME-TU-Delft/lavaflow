d5deb03c60db2f1771f8df5f995a57e11c34fb2a
abel (tudelft) <a.debruijn-3@student.tudelft.nl>
2022-05-20 13:18:25 +0200

    Added: comments for svelte stores

--- svelte-app/src/lib/stores/gltfStore.ts
+++ svelte-app/src/lib/stores/gltfStore.ts
@@ -1,3 +1,10 @@
+/**
+ * Svelte stores, store reactive global data for more info:
+ * https://svelte.dev/docs#run-time-svelte-store
+ *
+ * This stores gltf data
+ */
+
 import { writable } from 'svelte/store';
 
 export const mountainGltf = writable<string>();
