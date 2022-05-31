9b11c1ade17888fc8184a1fc52366a624d33421d
abel (tudelft) <a.debruijn-3@student.tudelft.nl>
2022-05-31 14:12:14 +0200

    Try to fix aframe

--- svelte-app/src/routes/demo.svelte
+++ svelte-app/src/routes/demo.svelte
@@ -1,138 +1,46 @@
 <script lang="ts">
 	import NavigationButton from '$lib/components/NavigationButton.svelte';
 
-	import init, * as wasm from 'wasm';
-
 	import { onMount, onDestroy } from 'svelte';
-
-	import { hc_curves, hc_hierarchy } from '$lib/data/hardCoded';
-	//import { contourLines } from '$lib/stores/contourLineStore';
-	import type { BufferGeometry, Material, Mesh } from 'three';
-	import { DoubleSide } from 'three';
-	import { goto } from '$app/navigation';
 
 	let mounted: boolean;
 	let aframe: boolean;
-	let ar: boolean;
-	let gltfUrl: string;
-	$: ready = aframe && ar && mounted;
-
-	const arLoaded = () => (ar = true);
-	const aframeLoaded = () => {
-		aframe = true;
-
-		AFRAME.registerComponent('double-render', {
-			schema: { opacityFactor: { default: 0.5 } },
-			init: function () {
-				this.traverseMesh.bind(this);
-
-				this.el.addEventListener('model-loaded', () => {
-					this.traverseMesh();
-				});
-			},
-			traverseMesh: function () {
-				const mesh = this.el.getObject3D('mesh');
-
-				if (!mesh) return;
-
-				mesh.traverse((node) => {
-					if (node.type == 'Mesh') {
-						(node as Mesh<BufferGeometry, Material>).material.side = DoubleSide;
-						// (node as Mesh<BufferGeometry, Material>).material.opacity = 0.8;
-					}
-				});
-			}
-		});
-	};
+	let gltfUrl: string = '/output20.gltf';
+	$: ready = aframe && mounted; // TODO: mounted is always true when aframe is true
 
 	onMount(async () => {
-		// if (!$contourLines.curves?.length || !$contourLines.hierarchy?.length) {
-		// 	goto('/scan/mapscanning');
-		// }
-
-		//console.log($contourLines);
-
-		await init();
-
-		// Construct an OpenCV tree from the information that we retrieved from OpenCV
-		const tree = new wasm.OpenCVTree({
-			pixels_per_curve: hc_curves,
-			parent_relations: hc_hierarchy
-		});
-
-		// Load the api
-		const api = new wasm.ModelConstructionApi();
-
-		// Pass this tree to Rust, through the API
-		api.base(tree);
-
-		console.log('before gltf build');
-		// Construct the final model and extract the gltf and lava_paths
-		const model_construction_result = api.build();
-		const gltf = model_construction_result.gltf;
-		const lava_paths = model_construction_result.lava_paths;
-
-		// console.log($contourLines.curves);
-
-		// const settings = new wasm.ModelGenerationSettings(5, 50, 50, 50, 1.0);
-		// const gltf = wasm.generate_3d_model(tree, settings, 2, 0.7, 0.7, 4, 1, 30, 30, 10);
-		// const gltfBlob = new Blob([gltf], { type: 'application/json' });
-		// gltfUrl = URL.createObjectURL(gltfBlob);
-		// mounted = true;
-	});
-
-	onDestroy(() => {
-		delete AFRAME.components['double-render'];
+		mounted = true;
+		console.log('mounted')
 	});
 </script>
 
 <svelte:head>
 	{#if mounted}
-		<script src="https://aframe.io/releases/1.0.0/aframe.min.js" on:load={aframeLoaded}></script>
-		<script
-			src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js"
-			on:load={arLoaded}></script>
+		<script src="https://aframe.io/releases/1.0.0/aframe.min.js" on:load={() => (aframe = true)}></script>
 	{/if}
 </svelte:head>
 
 {#if ready}
-	<a-scene
-		embedded
-		arjs="trackingMethod: best; detectionMode: mono_and_matrix; matrixCodeType: 3x3;"
-	>
+	<a-scene embedded>
 		<div class="backButton">
 			<NavigationButton back to="/scan/mapscanning">Rescan image</NavigationButton>
 		</div>
-		<!-- <a-entity light="type: ambient; color: #CCC" /> -->
 
-		<a-marker type="barcode" value="1">
-			<a-light position="0 3 0" intensity="2" type="point" />
-			<a-light position="-3 3 4" intensity="2" type="point" />
+		<a-box position="0 1 0" material="opacity: 0.5;" color="red" />
 
-			<a-box position="0 1 0" material="opacity: 0.5;" color="red" />
+		<a-entity
+			gltf-model="url({gltfUrl})"
+			position="3 0 -5"
+			scale="0.00038 0.1 0.00038"
+			rotation="0 -90 0"
+			id="model"
+		/>
 
-			<a-entity
-				double-render
-				gltf-model="url({gltfUrl})"
-				position="1 0 -1"
-				scale="0.0038 0.0038 0.0038"
-				rotation="0 -90 0"
-				id="model"
-			/>
-		</a-marker>
-		<a-entity camera />
+		<a-camera look-controls />
 	</a-scene>
 {/if}
 
 <style>
-	:global(.a-canvas, #arjs-video) {
-		display: block;
-		width: 100% !important;
-		height: 100% !important;
-		margin: 0 !important;
-		object-fit: contain !important;
-	}
-
 	.backButton {
 		position: absolute;
 		top: 1rem;
