2425453f8d34b99b2967a908c1bc5a097646d140
abel (tudelft) <a.debruijn-3@student.tudelft.nl>
2022-06-15 09:17:35 +0200

    Fix: Overlay of instructions

--- svelte-app/src/lib/components/Instructions.svelte
+++ svelte-app/src/lib/components/Instructions.svelte
@@ -1,4 +1,3 @@
-
 <div class="instructions">
 	<h2>Good example of contour map</h2>
 	<div class="example" />
@@ -8,33 +7,47 @@
 	<div class="section">Drawing</div>
 	<div class="instruction">
 		<div class="title">Curves should have sufficient margin</div>
-		<div class="description">The curves should have sufficient margin between them, overlapping or close lines are hard to interpret</div>
+		<div class="description">
+			The curves should have sufficient margin between them, overlapping or close lines are hard to
+			interpret
+		</div>
 	</div>
 	<div class="instruction">
 		<div class="title">Drawing with dark pen</div>
-		<div class="description">To recognise your level curves, we prefer a dark and thick marker. Using a lighter pen will result in inaccurate results</div>
+		<div class="description">
+			To recognise your level curves, we prefer a dark and thick marker. Using a lighter pen will
+			result in inaccurate results
+		</div>
 	</div>
 	<div class="instruction">
 		<div class="title">Close your curves</div>
-		<div class="description">To avoid false readings, close your curves, dont leave any open-ended lines</div>
+		<div class="description">
+			To avoid false readings, close your curves, dont leave any open-ended lines
+		</div>
 	</div>
 
 	<div class="section">Scanning</div>
 	<div class="instruction">
 		<div class="title">Four corners visible</div>
-		<div class="description">To scan the level curves, we need to have the four corners visible</div>
+		<div class="description">
+			To scan the level curves, we need to have the four corners visible
+		</div>
 	</div>
 	<div class="instruction">
 		<div class="title">Drawing should not be too far</div>
-		<div class="description">Scanning works best from a distance of 0.5 meters from the piece of paper. 
-			For best the best result try to scan the paper as close to parallel as possible. 
-		    This is easiest if you put your drawing on a table instead of holding it.</div>
+		<div class="description">
+			Scanning works best from a distance of 0.5 meters from the piece of paper. For best the best
+			result try to scan the paper as close to parallel as possible. This is easiest if you put your
+			drawing on a table instead of holding it.
+		</div>
 	</div>
 	<div class="instruction">
 		<div class="title">Proper lighting</div>
-		<div class="description">Level curves are hard to distinguish from background in a low light setting. 
-			Try to use natual daylight or a propper ceiling light for best results. Be careful of sharp or dark 
-			shadows on the drawing, as they might get interpreted as part of your drawing.</div>
+		<div class="description">
+			Level curves are hard to distinguish from background in a low light setting. Try to use natual
+			daylight or a propper ceiling light for best results. Be careful of sharp or dark shadows on
+			the drawing, as they might get interpreted as part of your drawing.
+		</div>
 	</div>
 </div>
 
@@ -46,10 +59,6 @@
 	}
 
 	.instructions {
-		padding: 3rem 1rem 1rem;
-		color: var(--text-color);
-		height: 100%;
-		overflow: auto;
 		max-width: 30rem;
 		margin: 0 auto;
 
