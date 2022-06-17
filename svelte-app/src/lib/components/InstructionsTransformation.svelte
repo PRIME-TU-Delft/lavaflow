b5ee49d124cb63abec1e63261b26309e061dfa98
Rens Dur <r.c.m.dur@student.tudelft.nl>
2022-06-17 17:48:09 +0200

    Spell check the instructions
    The instructions contained some spelling mistakes

--- svelte-app/src/lib/components/InstructionsTransformation.svelte
+++ svelte-app/src/lib/components/InstructionsTransformation.svelte
@@ -4,13 +4,13 @@
 
 	<div class="instruction">
 		Drag the shapes on your screen to the corresponding shapes on the template. 
-		Make sure that you don't leave part of your drawing outside the lines it makes.
+		Make sure that you don't leave part of your drawing outside the selected area.
 	</div>
 
 	<h2>Fixing your scan</h2>
 	<div>
 		If your scan does not work, you can move the shapes slightly, which could
-		fix it. Scanning works best if the draggable shapes are on the inside of the
+		help. Scanning works best if the draggable shapes are on the inside of the
 		rectangle formed by the shapes on the template, rather than outside or on top
 		of it. If moving markers does not work, try taking a new picture.</div>
 	</div>
