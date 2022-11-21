fa5c77a7dfd6b23a2ed3dac6edaf04cd979616cc
Abel de Bruijn <abeldebruijn@hotmail.com>
2022-11-21 23:23:16 +0100

    added: scanning, transform and preview

---
+++ svelte-app/src/routes/scan/mapscanning/+page.svelte
@@ -0,0 +1,95 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	import Button from '$lib/components/Button.svelte';
+	import NavigationButton from '$lib/components/NavigationButton.svelte';
+	import Dropdown from '$lib/components/input/Dropdown.svelte';
+	import Instructions from '$lib/components/Instructions.svelte';
+	import Page from '$lib/components/Page.svelte';
+	import Modal from '$lib/components/Modal.svelte';
+	import VideoStream from '$lib/components/VideoStream.svelte';
+	import Video from '$lib/components/Video.svelte';
+
+	import { mdiCamera, mdiHelpCircle } from '@mdi/js';
+	import { goto } from '$app/navigation';
+	import { rawImage } from '$lib/stores/imageStore';
+
+	let instructionVisible: boolean = false;
+	let videoSource: HTMLVideoElement;
+	let cameraSelected: MediaDeviceInfo;
+	let canvas: HTMLCanvasElement;
+	let loadingNextPage: boolean = false;
+	let deviceId: string;
+
+	const toggleInstruction = () => (instructionVisible = !instructionVisible);
+
+	/**
+	 * Goto scan/maptransform
+	 */
+	async function gotoTransform() {
+		loadingNextPage = true;
+
+		// Create an atificial canvas element
+		const context = canvas.getContext('2d');
+		canvas.height = videoSource.videoHeight;
+		canvas.width = videoSource.videoWidth;
+
+		// Take screenshot of video
+		if (context) context.drawImage(videoSource, 0, 0, canvas.width, canvas.height);
+		const image = canvas.toDataURL('image/png');
+
+		// Set image in (raw)image store
+		rawImage.set(image);
+
+		goto('/scan/maptransform');
+	}
+
+	function updateCamera() {
+		deviceId = cameraSelected.deviceId;
+	}
+</script>
+
+<Modal title="scan instructions" bind:visible={instructionVisible}>
+	<Instructions />
+</Modal>
+
+<VideoStream {deviceId} let:loading let:stream let:error let:cameraOptions>
+	<Page fullscreen={!loading}>
+		<NavigationButton slot="headerButton" back to="/">Back home</NavigationButton>
+
+		<svelte:fragment slot="background">
+			<Video {loading} {stream} />
+		</svelte:fragment>
+
+		<div slot="options">
+			{#if !instructionVisible && cameraOptions?.length > 1}
+				<Dropdown
+					bind:value={cameraSelected}
+					options={cameraOptions}
+					let:option
+					let:index
+					on:change={updateCamera}
+				>
+					{option.label || 'Camera ' + (index + 1)}
+				</Dropdown>
+			{/if}
+		</div>
+
+		<Video bind:videoSource --corner-radius="1rem" {error} {loading} {stream} />
+
+		<svelte:fragment slot="footer">
+			{#if !instructionVisible}
+				<Button secondary icon={mdiHelpCircle} on:click={toggleInstruction}>
+					Image capture | Instructions
+				</Button>
+			{/if}
+			<Button loading={loadingNextPage} icon={mdiCamera} on:click={gotoTransform}>
+				Capture photo of map
+			</Button>
+		</svelte:fragment>
+	</Page>
+</VideoStream>
+
+<canvas id="captureCanvas" bind:this={canvas} style="display: none; position: absolute" />
---
+++ svelte-app/src/routes/scan/preview/+page.svelte
@@ -0,0 +1,81 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+	/**
+	 * This is where the user inspects the level curves that are drawn, to check if the user wants to rescan the image or not
+	 */
+	import P5CurvesDebugView from '$lib/components/p5/P5CurvesDebugView.svelte';
+	import Button from '$lib/components/Button.svelte';
+	import Icon from '$lib/components/Icon.svelte';
+	import Page from '$lib/components/Page.svelte';
+	import NavigationButton from '$lib/components/NavigationButton.svelte';
+
+	import { mdiChevronRight } from '@mdi/js';
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	import { contourLines } from '$lib/stores/contourLineStore';
+	import { gltfStore } from '$lib/stores/gltfStore';
+	import { difficultyStore } from '$lib/stores/difficultyStore';
+	import { debugMode } from '$lib/stores/debugStore';
+
+	let foregroundWidth: number;
+	let foregroundHeight: number;
+
+	let gltfLoaded = false;
+
+	onMount(async () => {
+		if (!$contourLines.curves || !$contourLines.hierarchy) {
+			return goto('/scan/mapscanning');
+		}
+		await gltfStore.setup($contourLines, $difficultyStore.lava_forking);
+		gltfStore.build($contourLines);
+
+		gltfLoaded = true;
+	});
+</script>
+
+<Page>
+	<NavigationButton slot="headerButton" to="/scan/maptransform" back>
+		Back to selection
+	</NavigationButton>
+
+	<div slot="background" style="background:#aaa;" />
+
+	{#if $contourLines?.curves && $contourLines?.hierarchy}
+		<div class="sketch" bind:clientWidth={foregroundWidth} bind:clientHeight={foregroundHeight}>
+			<P5CurvesDebugView
+				debugMode={$debugMode}
+				curves={$contourLines.curves}
+				hierarchy={$contourLines.hierarchy}
+				{foregroundHeight}
+				{foregroundWidth}
+			/>
+		</div>
+	{/if}
+
+	<svelte:fragment slot="footer">
+		<Button secondary loading={!gltfLoaded} on:click={() => goto('/visualise/model')}>
+			<span>Visualise as 3D model</span>
+			<Icon path={mdiChevronRight} />
+		</Button>
+		<Button loading={!gltfLoaded} on:click={() => goto('/visualise/ar')}>
+			<span>Visualise in AR</span>
+			<Icon path={mdiChevronRight} />
+		</Button>
+	</svelte:fragment>
+</Page>
+
+<style>
+	.sketch {
+		height: 100%;
+		width: 100%;
+		position: absolute;
+		top: 0;
+		touch-action: none;
+		border-radius: 0.5rem;
+		overflow: hidden;
+	}
+</style>
