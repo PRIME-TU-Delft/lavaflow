b44c055c07737f7ff5aa6096ff72be1fc046ade9
Abel de Bruijn <abeldebruijn@hotmail.com>
2022-11-22 14:27:13 +0100

    sortof fixed wasm importing

--- svelte-app/vite.config.ts
+++ svelte-app/vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import wasmPack from 'vite-plugin-wasm-pack';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [wasmPack(['./../wasm']), sveltekit()]
 };
 
 export default config;
