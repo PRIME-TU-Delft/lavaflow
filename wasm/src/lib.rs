88a61cb997d53d74c56042fccc87e304b6baad26
Julia van der Kris <julia@juuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuulia.dev>
2022-12-31 01:09:11 +0100

    Remove wee_alloc dependency, as it is no longer maintained

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -8,12 +8,6 @@ mod api;
 mod api_helper_fns;
 mod gltf_conversion;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 extern "C" {
 	fn alert(s: &str);
