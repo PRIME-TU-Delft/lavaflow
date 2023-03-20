8f3e805c5294483aa629210f5962e03bf289fad5
Abel de Bruijn <abeldebruijn@hotmail.com>
2023-03-20 10:49:12 +0100

    added: aframe

--- app/src/routes/visual/ar/+page.svelte
+++ app/src/routes/visual/ar/+page.svelte
@@ -1,11 +1,16 @@
-<script>
-	import { Canvas } from '@threlte/core';
+<script lang="ts">
+	import 'aframe';
 	import VisualiseUi from '../VisualiseUI.svelte';
 	import Scene from './Scene.svelte';
+
+	const scale = [0.05, 0.025, 0.05] as [number, number, number];
 </script>
 
 <VisualiseUi title="Visual ar" baseUrl="/ar" let:showLava>
-	<Canvas>
-		<Scene {showLava} />
-	</Canvas>
+	<!-- TODO: customise ui buttons -->
+	<a-scene webxr="optionalFeatures: dom-overlay; overlayElement: #overlay;">
+		<Scene {scale} {showLava} />
+
+		<a-camera position="0 3 3" />
+	</a-scene>
 </VisualiseUi>
--- app/src/routes/visual/ar/Scene.svelte
+++ app/src/routes/visual/ar/Scene.svelte
@@ -1,5 +1,60 @@
 <script lang="ts">
-	export let showLava = false;
+	import { gltfStore } from '$lib/stores/gltfStore';
+	import { turbineLocations } from '$lib/stores/locationStore';
+
+	export let rotation = 0;
+	export let scale: [number, number, number];
+	export let showLava: boolean = false;
+
+	$: scaleString = scale.join(' ');
 </script>
 
-{showLava}
+{#if $gltfStore}
+	<!----------------------------------------------------------------
+	--        				SUPER ENTITY																	--
+	------------------------------------------------------------------>
+	<a-entity scale={scaleString} rotation="0 {rotation} 0">
+		<a-entity position="-50 0 -50">
+			<!----------------------------------------------------------------
+			--  Place "soccer" spotlights on each corner of the model				--
+			------------------------------------------------------------------>
+			<a-entity light="color: #ddf; intensity: 0.75" position="-100 150 -100" />
+			<a-entity light="color: #ddf; intensity: 0.75" position="100 150 -100" />
+			<a-entity light="color: #ddf; intensity: 0.75" position="-100 150 100" />
+			<a-entity light="color: #ddf; intensity: 0.75" position="100 150 100" />
+
+			<!----------------------------------------------------------------
+			--                    MOUNTAIN																	--
+			------------------------------------------------------------------>
+			<a-entity gltf-model="url({$gltfStore.gltf_url})" />
+			{#if showLava}
+				<!--LAVA gltf model-->
+				<a-entity gltf-model="url({$gltfStore.lava_gltf_url})" />
+			{/if}
+
+			<!----------------------------------------------------------------
+			--       		        	TARGETS				                					  --
+			--                                                              --
+			-- Loop over each target location. First each          					--
+			-- Draggable item is converted to an AltitudeAndGradient Object --
+			------------------------------------------------------------------>
+			{#each $turbineLocations.map((l) => gltfStore.getAlitituteAndGradient(l)) as altAndGrad}
+				<a-entity
+					gltf-model="url(/steam_turbine.glb)"
+					scale="0.1 0.2 0.1"
+					position="{altAndGrad.x} {altAndGrad.altitude} {altAndGrad.y}"
+					rotation="0 0 0"
+					animation-mixer
+				/>
+				<a-box
+					width="1.4"
+					depth="2.5"
+					height={altAndGrad.altitude / 2}
+					position="{altAndGrad.x} {altAndGrad.altitude / 2} {altAndGrad.y + 0.15}"
+					color="#444"
+					scale="1 2 1"
+				/>
+			{/each}
+		</a-entity>
+	</a-entity>
+{/if}
