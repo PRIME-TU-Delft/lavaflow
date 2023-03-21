b947e6018d3dc09aba0b3590aa777f84c42ddcc5
Abel de Bruijn <abeldebruijn@hotmail.com>
2023-03-21 16:25:56 +0100

    added: new homepage styling

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,48 +1,84 @@
 <script lang="ts">
+	import ActionButton from '$lib/components/ActionButton.svelte';
 	import LabeledButton from '$lib/components/LabeledButton.svelte';
-	import Section from '$lib/components/Section.svelte';
 	import VStack from '$lib/components/VStack.svelte';
-	import { mdiDownload } from '@mdi/js';
+	import { mdiChevronDoubleRight, mdiDownload } from '@mdi/js';
 	import Header from './Header.svelte';
+	import UserProfile from './UserProfile.svelte';
 </script>
 
 <Header />
 
 <VStack>
-	<Section>
+	<div class="prose mx-auto mt-12 p-8">
 		The climate crisis is upon us! Lava tends to very precisely follow the steepest downwards
 		direction, when it flows down the hills of a volcano. Its intense heat makes for a great
 		opportunity to generate electricity for nearby cities. Your job is to predict where the lava
 		will flow and place steam turbines on its paths. The steam turbines generate higher amounts of
 		electricity as the lava reaches closer. Save the world by using this amazing sustainable
 		energy-source!
-	</Section>
 
-	<Section>
-		<!-- TODO: onclick -->
-		<LabeledButton icon={mdiDownload}>
-			<p slot="label">
-				Alongside this applet, a printable template was designed on which you can draw your
-				level-curves.
-			</p>
-			Download template
-		</LabeledButton>
-
-		<!-- TODO: onclick -->
-		{#if true}
-			<LabeledButton icon={mdiDownload}>
-				<p slot="label">
-					We have detected that your device does not support webXR natively. The app is usable,
-					however we would recommend you to download a special AR browser.
-				</p>
-				Download webXR browser
-			</LabeledButton>
-		{/if}
-
-		<div class="pt-3">
-			<a href="/capture/instructions">Start</a>
+		<div class="mt-4 flex justify-end">
+			<ActionButton
+				twClass="no-underline"
+				href="/capture/instructions"
+				icon={mdiChevronDoubleRight}
+			>
+				Start lava app
+			</ActionButton>
 		</div>
-	</Section>
+	</div>
 
-	<Section title="Credits" bordered />
+	<div class="prose mx-auto mt-8 px-4">
+		<h2>Instructions:</h2>
+		<div class=" rounded-xl bg-red-100 p-4">
+			<!-- TODO: onclick -->
+			<LabeledButton icon={mdiDownload}>
+				<div slot="label">
+					Alongside this applet, a printable template was designed on which you can draw your
+					level-curves.
+				</div>
+				Download template
+			</LabeledButton>
+
+			{#if true}
+				<!-- TODO: onclick -->
+				<LabeledButton icon={mdiDownload}>
+					<div class="mt-4" slot="label">
+						We have detected that your device does not support webXR natively. The app is usable,
+						however we would recommend you to download a special AR browser.
+					</div>
+					Download webXR browser
+				</LabeledButton>
+			{/if}
+		</div>
+	</div>
+
+	<div class="prose mx-auto mt-12 mb-12 rounded-xl border-2 border-red-200 p-4">
+		<h2>Credits</h2>
+
+		<div class="">
+			<UserProfile
+				name="Abel de Bruijn"
+				image="adebruijn.jpeg"
+				title="Developer: User Interface and Augmented Reality"
+			/>
+			<UserProfile
+				name="Rens Dur"
+				image="rdur.jpeg"
+				title="Developer: Model Construction and Image Recognition"
+			/>
+			<UserProfile name="Pauline Hengst" title="Developer: Model Construction and Model Graphics" />
+			<UserProfile
+				name="Julia van de Kris"
+				image="jvanderkris.jpg"
+				title="'Full-stack developer'"
+			/>
+			<UserProfile
+				name="Jonas van Marrewijk"
+				image="jvanmarrewijk.jpeg"
+				title="Developer: Image Recognition"
+			/>
+		</div>
+	</div>
 </VStack>
---
+++ app/src/routes/Header.svelte
@@ -0,0 +1,38 @@
+<div class="relative max-h-96">
+	<img
+		class="h-full max-h-96 w-full rounded-bl-3xl rounded-br-3xl object-cover saturate-50"
+		src="volcano.jpg"
+		alt="title artwork"
+	/>
+
+	<a
+		href="https://www.tudelft.nl/ewi/over-de-faculteit/afdelingen/applied-mathematics/studeren/prime"
+		rel="noopener noreferrer"
+		target="_blank"
+		class="link left-4"
+	>
+		<img class="h-20  w-36 object-contain" src="primeLogo.svg" alt="prime logo" />
+	</a>
+
+	<a href="https://www.tudelft.nl" rel="noopener noreferrer" target="_blank" class="link right-4">
+		<img class="h-20 w-36 object-contain" src="TU_P5_white.png" alt="TU Delft logo" />
+	</a>
+
+	<div class="title absolute top-1/2 w-full text-center text-5xl font-bold text-white">
+		AR: Lava Flow
+	</div>
+</div>
+
+<style lang="postcss">
+	.link {
+		@apply absolute  top-4 z-10 rounded transition-colors md:bg-slate-500/10 md:p-4 md:backdrop-blur-md;
+	}
+
+	.link:hover {
+		@apply md:bg-slate-500/50;
+	}
+
+	.title {
+		font-family: 'Roboto Slab';
+	}
+</style>
