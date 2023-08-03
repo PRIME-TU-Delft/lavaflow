34677bceb6bb2011178663fcd68b39f7a62b253d
Abel de Bruijn <abeldebruijn@hotmail.com>
2023-08-03 14:45:21 +0200

    reintroduce rotate scene

--- svelte-app/src/routes/visual/ar/+page.svelte
+++ svelte-app/src/routes/visual/ar/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import 'aframe';
 	import type { Scene as AScene } from 'aframe';
-	import VisualiseUi from '../VisualiseUI.svelte';
+	import VisualiseUI from '../VisualiseUI.svelte';
 	import Scene from './Scene.svelte';
 	import Button from '$lib/components/Button.svelte';
 
 	let scale = 2;
+	let rotate = 0;
 	let sceneEl: AScene;
 
 	const baseUrl = '/ar';
@@ -16,18 +17,23 @@
 	}
 </script>
 
-<VisualiseUi title="Visual AR" {baseUrl} let:showLava>
+<VisualiseUI title="Visual AR" {baseUrl} let:showLava>
 	<a-scene
 		bind:this={sceneEl}
 		vr-mode-ui="enterVRButton: #myEnterVRButton; enterARButton: #myEnterARButton"
 		webxr="optionalFeatures: dom-overlay; overlayElement: #overlay;"
 	>
-		<Scene {scale} {showLava} />
+		<Scene rotation={rotate} {scale} {showLava} />
 
 		<a-camera position="0 3 3" />
 	</a-scene>
 
 	<div slot="arActions">
+		<div class="flex gap-2">
+			<div>Rotate:</div>
+			<input type="range" min="-200" max="200" bind:value={rotate} class="range range-primary" />
+		</div>
+
 		<div class="hidden" id="myEnterVRButton">Open vr</div>
 
 		<Button twClass="enterAR w-full" on:click={() => (scale = 0.5)} id="myEnterARButton">
@@ -36,7 +42,7 @@
 
 		<Button twClass="closeAR hidden w-full" on:click={closeAR}>Close AR Mode</Button>
 	</div>
-</VisualiseUi>
+</VisualiseUI>
 
 <style>
 	:global(#overlay:xr-overlay .enterAR) {
