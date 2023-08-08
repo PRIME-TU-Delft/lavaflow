2881ce35a74144f0d35b25bbf31f0fc9889a94ae
Abel de Bruijn <abeldebruijn@hotmail.com>
2023-08-09 00:00:18 +0200

    clean up and test python version

--- svelte-app/src/routes/document-scanning/+page.svelte
+++ svelte-app/src/routes/document-scanning/+page.svelte
@@ -1,28 +1,16 @@
 <script lang="ts">
 	import Video from '$lib/components/Video.svelte';
 	import Menubar from '$lib/components/Menubar.svelte';
-
-	import * as gm from 'gammacv';
 	import Dropdown from '$lib/components/Dropdown.svelte';
-
-	import cv2 from 'opencv-ts';
+	import cv from 'opencv-ts';
 
 	let height: number; // window height
 	let width: number; // window width
 
 	let deviceId: string; // Id that references the current camera
-	let session: gm.Session; // GammaCV session
-	let linesSession: gm.Session; // GammaCV session, specifically for the lines
-	let stream: gm.CaptureVideo; // Video stream
-	let bwPipeline: gm.Operation;
-	let linesPipeline: gm.Operation;
-
 	let videoSource: HTMLVideoElement;
-
 	let tickLoop: number;
-
 	let linesCanvasEl: HTMLCanvasElement; // Canvas element to hold the lines
-	let outputCanvasEl: HTMLCanvasElement; // Canvas element to hold the output
 
 	const params = {
 		D_COEF: 2.0,
@@ -32,191 +20,71 @@
 
 	// Helper function: ones
 	function ones(num: number) {
-		let result = [];
+		return Array.from({ length: num }).map(() => 1);
+	}
 
-		for (let i = 0; i < num; i++) {
-			result.push(1);
-		}
-
-		return result;
+	function zeros(num: number) {
+		return Array.from({ length: num }).map(() => 0);
 	}
 
 	function documentDetectionWithOpenCv(canvas: HTMLCanvasElement) {
 		// Create an OpenCv matrix from the contents of the canvas
-		const img = cv2.imread(canvas);
+		let img = cv.imread(canvas);
 
-		// let step1 = new cv2.Mat();
-		// const kernel = cv2.matFromArray(5, 5, cv2.CV_32FC1, ones(5 * 5));
-		// cv2.morphologyEx(
-		// 	img,
-		// 	step1,
-		// 	cv2.MORPH_CLOSE,
-		// 	kernel,
-		// 	new cv2.Point(0, 0),
-		// 	3,
-		// 	cv2.BORDER_DEFAULT,
-		// 	new cv2.Scalar(0)
-		// );
+		// Step 1 - Remove details while keeping the edges
+		let step1 = new cv.Mat();
+		const kernel = cv.matFromArray(5, 5, cv.CV_32FC1, ones(5 * 5));
+		cv.morphologyEx(
+			img,
+			step1,
+			cv.MORPH_CLOSE,
+			kernel,
+			new cv.Point(0, 0),
+			3,
+			cv.BORDER_DEFAULT,
+			new cv.Scalar(0)
+		);
 
-		const gray = new cv2.Mat(); // create empty image for holding the grayscale image
-		cv2.cvtColor(img, gray, cv2.COLOR_RGBA2GRAY, 0); // convert image to grayscale
+		// Step 2 - The Magic of GrabCut
+		const mask = cv.matFromArray(width, height, cv.CV_8U, zeros(width * height)); // mask = np.zeros(img.shape[:2],np.uint8)
+		const bgdModel = cv.matFromArray(1, 65, cv.CV_64F, zeros(65)); // bgdModel = np.zeros((1,65),np.float64)
+		const fgdModel = cv.matFromArray(1, 65, cv.CV_64F, zeros(65)); // fgdModel = np.zeros((1,65),np.float64)
+		const rect = new cv.Rect(20, 20, height - 20, width - 20); // rect = (20,20,img.shape[1]-20,img.shape[0]-20)
+		// cv.grabCut(step1, mask, rect, bgdModel, fgdModel, 5, cv.GC_INIT_WITH_RECT); // cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+		// mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+		// img = img*mask2[:,:,np.newaxis]
 
-		cv2.imshow(canvas, gray);
+		cv.imshow(canvas, step1);
 	}
 
-	function getPipeline(input: gm.Tensor<gm.TensorDataView>) {
-		// Normalization: add contrast, make colors seem deeper
-		let pipeline = gm.norm(input, 'l2');
-		// Erosion: erode into rectangles of shape 2x2 (best to see for yourself: https://gammacv.com/examples/erode)
-		pipeline = gm.erode(pipeline, [2, 2]);
-		// Adaptive Threshold: Black/white - make pixels black if they pass the threshold 20 within a certain box of size 10
-		// (best to see for yourself: https://gammacv.com/examples/adaptive_threshold)
-		pipeline = gm.adaptiveThreshold(pipeline, 10, 35);
-		// Gaussian Blur: remove sharp edges
-		pipeline = gm.gaussianBlur(pipeline, 3, 1);
-		// Make the lines a bit thinner so the result from opencv's getContours is better
-		pipeline = gm.adaptiveThreshold(pipeline, 1);
-
-		return pipeline;
+	/** Recursive function that runs each frame */
+	function tick(frame: number = 0) {
+		tickLoop = requestAnimationFrame(() => tick(frame + 1));
 	}
 
-	function getLinesPipeline(input: gm.Tensor<gm.TensorDataView>) {
-		const params = {
-			D_COEF: 2.0,
-			LAYERS: 2,
-			LINE_COUNT: 10
-		};
-
-		let pipeline = gm.grayscale(input);
-		pipeline = gm.downsample(pipeline, params.D_COEF);
-		pipeline = gm.gaussianBlur(pipeline, 3, 3);
-		pipeline = gm.sobelOperator(pipeline);
-		pipeline = gm.cannyEdges(pipeline, 0.25, 0.75);
-		pipeline = gm.pcLines(pipeline, params.LAYERS, 2, 2);
-		return pipeline;
-	}
-
-	function drawLines(
-		input: gm.Tensor<gm.TensorDataView>,
-		output: gm.Tensor<gm.TensorDataView>,
-		canvasProcessed: HTMLCanvasElement
+	function start(
+		previewCanvas: HTMLCanvasElement,
+		deviceId?: string,
+		videoSource?: HTMLVideoElement
 	) {
-		let lines = [];
+		const context1 = previewCanvas.getContext('2d');
 
-		for (let i = 0; i < output.size / 4; i += 1) {
-			const y = Math.floor(i / output.shape[1]);
-			const x = i - y * output.shape[1];
-			const value = output.get(y, x, 0);
-			const x0 = output.get(y, x, 1);
-			const y0 = output.get(y, x, 2);
+		if (!context1 || !videoSource)
+			return console.error('No context or video source', context1, videoSource);
 
-			if (value > 0.0) {
-				lines.push([value, x0, y0]);
-			}
-		}
-
-		// Sort the lines from best to worst and pick the first LINE_COUNT lines
-		lines = lines.sort((b, a) => a[0] - b[0]);
-		lines = lines.slice(0, params.LINE_COUNT);
-
-		const lineContext = new gm.Line();
-
-		const maxP = Math.max(input.shape[0], input.shape[1]);
-		for (let i = 0; i < lines.length; i += 1) {
-			lineContext.fromParallelCoords(
-				lines[i][1] * params.D_COEF,
-				lines[i][2] * params.D_COEF,
-				input.shape[1],
-				input.shape[0],
-				maxP,
-				maxP / 2
-			);
-
-			gm.canvasDrawLine(canvasProcessed, lineContext, 'rgba(0, 255, 0, 1.0)');
-		}
-	}
-
-	function tick(
-		input: gm.Tensor<gm.TensorDataView>,
-		canvas: HTMLCanvasElement,
-		contexts: CanvasRenderingContext2D[],
-		frame: number = 0
-	) {
-		tickLoop = requestAnimationFrame(() => tick(input, canvas, contexts, frame + 1));
-
-		//
-		// Part 1: Transform to BW image
-		//
-		stream.getImageBuffer(input);
-
-		// Create an output tensor
-		const gammacvOutputTensor = gm.tensorFrom(bwPipeline);
-
-		// Guard against null of gammacvOutputTensor
-		if (!gammacvOutputTensor) return;
-
-		// Run the pipeline on this session
-		session.runOp(bwPipeline, frame, gammacvOutputTensor);
-
-		// Fill the provided canvas with the output of this operation
-		gm.canvasFromTensor(canvas, gammacvOutputTensor);
-
-		contexts[0].drawImage(canvas, 0, 0, width, height);
-
-		//
-		// Part 2: Display the lines that are extracted by GammaCV
-		//
-
-		// const gammacvLinesTensor = gm.tensorFrom(linesPipeline);
-
-		// if (!gammacvLinesTensor) return;
-
-		// // gm.tensorClone(gammacvOutputTensor, gammacvLinesTensor);
-
-		// // Run the pipeline on this session
-		// linesSession.runOp(linesPipeline, frame, gammacvLinesTensor);
-
-		// // Fill the right canvas with the output of this operation
-		// gm.canvasFromTensor(canvas, gammacvLinesTensor);
-
-		// contexts[1].drawImage(canvas, 0, 0, width, height);
-
-		// drawLines(gammacvOutputTensor, gammacvLinesTensor, linesCanvasEl);
-	}
-
-	function start(previewCanvas: HTMLCanvasElement, deviceId?: string) {
-		// if (!previewCanvas) return;
-		// if (!session) session = new gm.Session();
-		// if (!linesSession) linesSession = new gm.Session();
-		// if (tickLoop) cancelAnimationFrame(tickLoop);
-
-		// stream = new gm.CaptureVideo(width, height);
-		// stream.start(deviceId);
-		const context1 = linesCanvasEl.getContext('2d');
-		context1?.drawImage(videoSource, 0, 0, width, height);
-
-		// const input = new gm.Tensor('uint8', [height, width, 4]);
-		// bwPipeline = getDocumentDetectionPipeline(input); // define bw pipeline
-		// linesPipeline = getLinesPipeline(input);
-		// // Initialise the sessions
-		// session.init(bwPipeline); // initialize bw pipeline
-		// linesSession.init(linesPipeline);
-		// const canvas = gm.canvasCreate(width, height);
-		// const context0 = previewCanvas.getContext('2d');
-		// const context1 = linesCanvasEl.getContext('2d');
-		// if (!context0 || !context1) return;
-		// tick(input, canvas, [context0, context1]);
-		// canvas.remove();
-
+		context1.drawImage(videoSource, 0, 0, width, height);
 		documentDetectionWithOpenCv(linesCanvasEl);
 	}
 
 	function setCameraId(label: string) {
 		deviceId = label;
 
-		if (stream) stream.stop();
+		start(linesCanvasEl, deviceId);
+	}
 
-		start(outputCanvasEl, deviceId);
+	$: if (videoSource) {
+		console.log('loaded');
+		start(linesCanvasEl, deviceId, videoSource);
 	}
 </script>
 
@@ -233,15 +101,10 @@
 				{/each}
 			</Dropdown>
 		{/if}
-	</Menubar>
 
-	{cameraOptions.map((c) => c.label)}
+		<button class="btn" on:click={() => start(linesCanvasEl, deviceId, videoSource)}>Tick</button>
+	</Menubar>
 
 	<!-- Lines canvas -->
 	<canvas {width} {height} bind:this={linesCanvasEl} />
-
-	<!-- Output canvas -->
-	{#if linesCanvasEl}
-		<canvas {width} {height} bind:this={outputCanvasEl} use:start={deviceId} />
-	{/if}
 </Video>
