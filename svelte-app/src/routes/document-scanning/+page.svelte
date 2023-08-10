0e9bc057acf4c901fb9c8b9ca19f916f86141e7f
Rens Dur <r.c.m.dur@student.tudelft.nl>
2023-08-10 14:52:33 +0200

    Implement new pipeline

--- svelte-app/src/routes/document-scanning/+page.svelte
+++ svelte-app/src/routes/document-scanning/+page.svelte
@@ -2,7 +2,7 @@
 	import Video from '$lib/components/Video.svelte';
 	import Menubar from '$lib/components/Menubar.svelte';
 	import Dropdown from '$lib/components/Dropdown.svelte';
-	import cv2 from 'opencv-ts';
+	import * as gm from 'gammacv';
 
 	let height: number; // window height
 	let width: number; // window width
@@ -11,6 +11,10 @@
 	let videoSource: HTMLVideoElement;
 	let tickLoop: number;
 	let linesCanvasEl: HTMLCanvasElement; // Canvas element to hold the lines
+
+	// GammaCV variables
+	let gmStream: gm.CaptureVideo;
+	let gmSession: gm.Session;
 
 	const params = {
 		D_COEF: 2.0,
@@ -27,64 +31,38 @@
 		return Array.from({ length: num }).map(() => 0);
 	}
 
-	function documentDetectionWithOpenCv(canvas: HTMLCanvasElement) {
-		// Create an OpenCv matrix from the contents of the canvas
-		let img = cv2.imread(canvas);
+	async function documentDetection(canvas: HTMLCanvasElement, frame: number = 0) {
+		// Read the data from the stream
+		let tensor = new gm.Tensor('uint8', [height, width, 4]);
+		gmStream.getImageBuffer(tensor);
 
-		// Step 1 - Remove details while keeping the edges
-		const kernel = cv2.matFromArray(5, 5, cv2.CV_32FC1, ones(5 * 5));
-		cv2.morphologyEx(
-			img,
-			img,
-			cv2.MORPH_CLOSE,
-			kernel,
-			new cv2.Point(0, 0),
-			3,
-			cv2.BORDER_DEFAULT,
-			new cv2.Scalar(0)
-		);
+		// Define the image processing pipeline
 
-		// Step 2 - The Magic of GrabCut
-		const mask = cv2.matFromArray(width, height, cv2.CV_8U, zeros(width * height)); // mask = np.zeros(img.shape[:2],np.uint8)
-		const bgdModel = cv2.matFromArray(1, 65, cv2.CV_64F, zeros(65)); // bgdModel = np.zeros((1,65),np.float64)
-		const fgdModel = cv2.matFromArray(1, 65, cv2.CV_64F, zeros(65)); // fgdModel = np.zeros((1,65),np.float64)
-		const rect = new cv2.Rect(20, 20, width - 40, height - 40); // rect = (20,20,img.shape[1]-20,img.shape[0]-20)
-		cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT); // cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
+		// Dilation
+		let pipeline = gm.dilate(tensor, [20, 20]);
+		// Normalisation
+		pipeline = gm.gaussianBlur(pipeline, 10, 10);
+		pipeline = gm.norm(pipeline, 'l2');
+		pipeline = gm.threshold(pipeline, 0.3, 0);
+		// pipeline = gm.colorSegmentation(pipeline, 2);
 
-		//
-		// Draw the foreground after grabCut
-		//
+		// Extract the tensor output
+		const pipelineTensor: any = gm.tensorFrom(pipeline);
 
-		// For every pixel in the Mat
-		for (let i = 0; i < img.rows; i++) {
-			for (let j = 0; j < img.cols; j++) {
-				// If the pixel value in 'mask' is either 0 or 2
-				if (mask.ucharPtr(i, j)[0] == 0 || mask.ucharPtr(i, j)[0] == 2) {
-					img.ucharPtr(i, j)[0] = 0;
-					img.ucharPtr(i, j)[1] = 0;
-					img.ucharPtr(i, j)[2] = 0;
-				}
-			}
-		}
+		// Create and initialize the GammaCV session, to acquire GPU acceperation
+		gmSession.init(pipeline);
 
-		// Draw the grab rectangle
-		const whiteColor = new cv2.Scalar(0, 0, 255);
-		const point1 = new cv2.Point(rect.x, rect.y);
-		const point2 = new cv2.Point(rect.x + rect.width, rect.y + rect.height);
-		cv2.rectangle(img, point1, point2, whiteColor);
+		// Run the canny-edges operation
+		gmSession.runOp(pipeline, frame, pipelineTensor);
 
-		cv2.imshow(canvas, img);
-
-		// Delete all objects
-		img.delete();
-		mask.delete();
-		bgdModel.delete();
-		fgdModel.delete();
+		gm.canvasFromTensor(canvas, pipelineTensor);
 	}
 
 	/** Recursive function that runs each frame */
-	function tick(frame: number = 0) {
-		tickLoop = requestAnimationFrame(() => tick(frame + 1));
+	function tick(canvas: HTMLCanvasElement, frame: number = 0) {
+		tickLoop = requestAnimationFrame(() => tick(canvas, frame + 1));
+
+		documentDetection(canvas, frame);
 	}
 
 	function start(
@@ -92,24 +70,32 @@
 		deviceId?: string,
 		videoSource?: HTMLVideoElement
 	) {
+		// Setup the GM stream
+		gmStream = new gm.CaptureVideo(width, height);
+		gmStream.start(deviceId);
+		// Setup the GM session
+		gmSession = new gm.Session();
+
 		const context1 = previewCanvas.getContext('2d');
 
 		if (!context1 || !videoSource)
 			return console.error('No context or video source', context1, videoSource);
 
 		context1.drawImage(videoSource, 0, 0, width, height);
-		documentDetectionWithOpenCv(previewCanvas);
+		documentDetection(previewCanvas);
+
+		tick(previewCanvas, 0);
 	}
 
 	function setCameraId(label: string) {
 		deviceId = label;
 
-		// start(linesCanvasEl, deviceId);
+		start(linesCanvasEl, deviceId);
 	}
 
 	$: if (videoSource) {
 		console.log('loaded');
-		// start(linesCanvasEl, deviceId, videoSource);
+		start(linesCanvasEl, deviceId, videoSource);
 	}
 </script>
 
@@ -131,5 +117,5 @@
 	</Menubar>
 
 	<!-- Lines canvas -->
-	<canvas {width} {height} bind:this={linesCanvasEl} />
+	<canvas style="position: absolute; top: 0;" {width} {height} bind:this={linesCanvasEl} />
 </Video>
