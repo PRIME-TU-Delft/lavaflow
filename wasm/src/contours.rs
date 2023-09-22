87f1f7b76f55a5deb098c96de21c748006b0c2f0
Rens Dur <r.c.m.dur@student.tudelft.nl>
2023-09-22 13:44:15 +0200

    Start work on hierarchy

--- wasm/src/contours.rs
+++ wasm/src/contours.rs
@@ -1,22 +1,18 @@
-use contour_isobands::{ContourBuilder, Band};
-use geojson::{GeoJson, FeatureCollection};
+use contour_isobands::{Band, ContourBuilder};
+use geojson::{FeatureCollection, GeoJson};
 use miette::{miette, Result};
 use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
 
-
 /// Converts a vector of bands to a GeoJSON string
 fn to_json(bands: &[Band]) -> String {
-	let features: Vec<geojson::Feature> = bands.iter()
-		.map(|band| band.to_geojson())
-		.collect();
+	let features: Vec<geojson::Feature> = bands.iter().map(|band| band.to_geojson()).collect();
 
-	GeoJson::from(
-		FeatureCollection {
-			bbox: None,
-			features,
-			foreign_members: None
-		}
-	).to_string()
+	GeoJson::from(FeatureCollection {
+		bbox: None,
+		features,
+		foreign_members: None,
+	})
+	.to_string()
 }
 
 /// Takes a 2D array of values and returns a GeoJSON of detected contours
@@ -25,28 +21,20 @@ pub fn contours(data: JsValue) -> Result<String, JsValue> {
 	let data: Vec<Vec<f64>> = serde_wasm_bindgen::from_value(data).unwrap();
 
 	// Transpose data vec
-	let data_transpose = data.clone().into_iter()
-		.enumerate()
-		.map(|(i, _)| data.iter().map(|row| row[i]).collect())
-		.collect::<Vec<Vec<f64>>>();
+	let data_transpose = data.clone().into_iter().enumerate().map(|(i, _)| data.iter().map(|row| row[i]).collect()).collect::<Vec<Vec<f64>>>();
 
 	// Flatten data vec
 	let data_flat: Vec<f64> = data_transpose.clone().into_iter().flatten().collect();
 
 	let len_x = data_transpose.len();
-	let len_y = data_transpose.get(0).
-		ok_or(miette!("Image has no width lol"))
-		.map_err(|e| e.to_string())?
-		.len();
+	let len_y = data_transpose.get(0).ok_or(miette!("Image has no width lol")).map_err(|e| e.to_string())?.len();
 
 	let contours = ContourBuilder::new(len_x, len_y)
 		.use_quad_tree(true)
 		.contours(&data_flat, &[0., 0.3, 1.])
-		.map_err(|e| {
-			miette!("Error in contour detection")
-				.context(e.to_string())
-				.to_string()
-		})?;
+		.map_err(|e| miette!("Error in contour detection").context(e.to_string()).to_string())?;
+
+	// So far, we've grouped the
 
 	Ok(to_json(&contours))
 }
