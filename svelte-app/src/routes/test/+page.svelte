9bfc2aaf6c5a83b30788cfa883d0df81ed107578
Abel de Bruijn <abeldebruijn@hotmail.com>
2023-09-22 11:23:35 +0200

    added visualiser

--- svelte-app/src/routes/test/+page.svelte
+++ svelte-app/src/routes/test/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import * as d3 from 'd3';
 	import { onMount } from 'svelte';
+	import { data as rawData } from './contours';
 
 	let width = 960;
 	let height = 500;
@@ -10,19 +11,22 @@
 	let svgEl: SVGSVGElement;
 	let gEl: SVGGElement;
 
-	const thresholds = Array.from({ length: 2 }, (_, i) => (i - 6) / 5);
+	const thresholds = Array.from({ length: 1 }, (_, i) => i / 2);
+	const value = (x: number, y: number) => Math.sin(x + y) * Math.sin(x - y);
 
 	function createGrid() {
 		const x0 = -q / 2,
 			x1 = width + q;
 		const y0 = -q / 2,
 			y1 = height + q;
-
-		// const n = Math.ceil((x1 - x0) / q);
-		// const m = Math.ceil((y1 - y0) / q);
-		const n = 43;
-		const m = 43;
-		const data: number[] = dataRaw.flat();
+		const n = Math.ceil((x1 - x0) / q);
+		const m = Math.ceil((y1 - y0) / q);
+		const data = new Array(n * m);
+		for (let j = 0; j < m; ++j) {
+			for (let i = 0; i < n; ++i) {
+				data[j * n + i] = value(i * q * scale, j * q * scale);
+			}
+		}
 
 		return { data, n, m };
 	}
@@ -35,35 +39,60 @@
 
 	const zoomProtocol = d3
 		.zoom<SVGSVGElement, unknown>()
-		.scaleExtent([1 / 3, 3])
+		.scaleExtent([1 / 3, 20])
 		.on('zoom', handleZoom);
 
+	let contourList: string[] = [];
 	onMount(() => {
 		let path = d3.geoPath();
 
-		const contours: d3.ContourMultiPolygon[] = [];
-		for (const v of thresholds) {
-			let contour = d3.contours().size([grid.n, grid.m]).contour(grid.data, v);
+		// const contours = d3.contours().size([grid.n, grid.m]);
 
-			contour.coordinates = contour.coordinates.map((coords) => {
-				return coords.map((c) => c.map(([x, y]) => [x * scale, y * scale]));
-				// 	coords.map((c) => c.map(([x, y]) => [x * 1000, y * 1000]));
-				// 	console.log({ coords });
-				// 	return coords;
-			});
-			console.log(contour);
+		const multiPolies: d3.ContourMultiPolygon[] = [];
 
-			contours.push(contour);
+		type Position = [number, number];
+		type D3ContourMultiPolygon = {
+			value: number;
+			coordinates: Position[][][];
+			type: 'MultiPolygon';
+		};
 
-			// path(contour);
+		const contourMultiPolygons: d3.ContourMultiPolygon[] = rawData.features.map((f) => {
+			return {
+				value: f.properties.max_v,
+				coordinates: f.geometry.coordinates,
+				type: 'MultiPolygon'
+			};
+		});
+
+		for (const polyPath of contourMultiPolygons) {
+			const pathString = path(polyPath);
+
+			if (!pathString) continue;
+
+			contourList = [...contourList, pathString];
 		}
 
-		d3.select(svgEl).call(zoomProtocol);
+		// for (const v of thresholds) {
+		// 	const pathString = path(contours.contour(grid.data, v));
 
-		d3.select(gEl).selectAll('path').data(contours).enter().append('path').attr('d', d3.geoPath());
+		// 	if (!pathString) continue;
+
+		// 	contourList = [...contourList, pathString];
+		// }
+
+		console.log(contourList);
+
+		d3.select(svgEl).call(zoomProtocol);
 	});
 </script>
 
+<svelte:window bind:innerHeight={height} bind:innerWidth={width} />
+
 <svg bind:this={svgEl} {width} {height}>
-	<g style="stroke-width: 0.01px" bind:this={gEl} />
+	<g style="stroke-width: 0.1px; stroke: white;" bind:this={gEl}>
+		{#each contourList as contour}
+			<path d={contour} fill="none" stroke="white" />
+		{/each}
+	</g>
 </svg>
