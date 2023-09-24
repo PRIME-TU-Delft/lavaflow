45173c10d28b1c02a35d01b0c541bad8a8e68dd0
Abel de Bruijn <abeldebruijn@hotmail.com>
2023-09-24 15:57:30 +0200

    understanding the datastructure

--- svelte-app/src/routes/test/+page.svelte
+++ svelte-app/src/routes/test/+page.svelte
@@ -5,33 +5,9 @@
 
 	let width = 960;
 	let height = 500;
-	const scale = 80;
-	const q = 6; // The level of detail, e.g., sample every 6 pixels in x and y.
 
 	let svgEl: SVGSVGElement;
-	let gEl: SVGGElement;
-
-	const thresholds = Array.from({ length: 1 }, (_, i) => i / 2);
-	const value = (x: number, y: number) => Math.sin(x + y) * Math.sin(x - y);
-
-	function createGrid() {
-		const x0 = -q / 2,
-			x1 = width + q;
-		const y0 = -q / 2,
-			y1 = height + q;
-		const n = Math.ceil((x1 - x0) / q);
-		const m = Math.ceil((y1 - y0) / q);
-		const data = new Array(n * m);
-		for (let j = 0; j < m; ++j) {
-			for (let i = 0; i < n; ++i) {
-				data[j * n + i] = value(i * q * scale, j * q * scale);
-			}
-		}
-
-		return { data, n, m };
-	}
-
-	const grid = createGrid();
+	let pathString: string | null;
 
 	function handleZoom(e: any) {
 		d3.select('svg g').attr('transform', e.transform);
@@ -42,46 +18,36 @@
 		.scaleExtent([1 / 3, 20])
 		.on('zoom', handleZoom);
 
-	let contourList: string[] = [];
 	onMount(() => {
 		let path = d3.geoPath();
 
-		// const contours = d3.contours().size([grid.n, grid.m]);
+		const lastPolygon = rawData.features.at(-1);
 
-		const multiPolies: d3.ContourMultiPolygon[] = [];
+		// Todo handle error - No polygons found in dataset
+		// @do - lower threshold
+		if (!lastPolygon) return;
 
-		type Position = [number, number];
-		type D3ContourMultiPolygon = {
-			value: number;
-			coordinates: Position[][][];
-			type: 'MultiPolygon';
+		let testCoordinates = lastPolygon.geometry.coordinates
+			.filter((cs) => {
+				// Coordinates of length 1 are not do not have a inner and outer polygon
+				// Coordinates of length 2 are the outer and inner polygon
+				// Coordinates of length > 2 are holes in the polygon
+				// return cs.length == 1;
+				return cs.length == 2;
+				// return cs.length > 2;
+			})
+			.map((cs) => [cs.at(0)])
+			.map((cs) => cs.map((c) => c?.map((cc) => [cc[0] * 5, cc[1] * 5]) || []));
+
+		let contourMultiPolygons: d3.ContourMultiPolygon = {
+			value: lastPolygon.properties.max_v,
+			coordinates: testCoordinates,
+			type: 'MultiPolygon'
 		};
 
-		const contourMultiPolygons: d3.ContourMultiPolygon[] = rawData.features.map((f) => {
-			return {
-				value: f.properties.max_v,
-				coordinates: f.geometry.coordinates,
-				type: 'MultiPolygon'
-			};
-		});
+		pathString = path(contourMultiPolygons);
 
-		for (const polyPath of contourMultiPolygons) {
-			const pathString = path(polyPath);
-
-			if (!pathString) continue;
-
-			contourList = [...contourList, pathString];
-		}
-
-		// for (const v of thresholds) {
-		// 	const pathString = path(contours.contour(grid.data, v));
-
-		// 	if (!pathString) continue;
-
-		// 	contourList = [...contourList, pathString];
-		// }
-
-		console.log(contourList);
+		if (!pathString) return; // Todo handle error - No path string found
 
 		d3.select(svgEl).call(zoomProtocol);
 	});
@@ -90,9 +56,7 @@
 <svelte:window bind:innerHeight={height} bind:innerWidth={width} />
 
 <svg bind:this={svgEl} {width} {height}>
-	<g style="stroke-width: 0.1px; stroke: white;" bind:this={gEl}>
-		{#each contourList as contour}
-			<path d={contour} fill="none" stroke="white" />
-		{/each}
+	<g style="stroke-width: 0.1px; stroke: white;">
+		<path d={pathString} fill="none" stroke="white" stroke-width="2px" />
 	</g>
 </svg>
