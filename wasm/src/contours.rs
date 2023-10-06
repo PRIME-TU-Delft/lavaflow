ab17b213c7ae50fe742165b5e2caec361001c31f
Julia van der Kris <julia@juuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuulia.dev>
2023-10-06 19:22:25 +0200

    This compiles, but using lots of clones feels kinda hacky (+ replace manual Clone impl for LevelCurve struct with derive)

--- wasm/src/contours.rs
+++ wasm/src/contours.rs
@@ -3,23 +3,22 @@ use geo::{Area, Contains, Polygon};
 use miette::{miette, Result};
 
 
-struct ContourHierarchy<'a> {
-	contour: &'a Polygon,
-	children: Vec<ContourHierarchy<'a>>,
+pub struct ContourHierarchy {
+	contour: Polygon,
+	children: Vec<ContourHierarchy>,
 }
 
 
-pub fn pipeline<'a>(data: Vec<Vec<f64>>) -> Result<ContourHierarchy<'a>> {
+pub fn pipeline(data: Vec<Vec<f64>>) -> Result<ContourHierarchy> {
 	let bands = contours(data)?;
 	let band = bands.last()
-		.ok_or(miette!("No contours detected in drawing"))?
-		.clone();
+		.ok_or(miette!("No contours detected in drawing"))?;
 
-	let filtered = band.geometry.iter()
+	let filtered = band.geometry.clone().into_iter()
 		.filter(|polygon| polygon.exterior().coords().collect::<Vec<_>>().len() > 10)  // Filter out small polygons
 		.collect::<Vec<_>>();
 
-	let hierarchy = hierarchy(&filtered)?;
+	let hierarchy = hierarchy(filtered)?;
 	Ok(hierarchy)
 }
 
@@ -33,13 +32,13 @@ fn contours(data: Vec<Vec<f64>>) -> Result<Vec<Band>> {
 		.map(|(i, _)| data.iter().map(|row| row[i]).collect())
 		.collect::<Vec<Vec<f64>>>();
 
-	// Flatten data vec
-	let data_flat: Vec<f64> = data_transpose.clone().into_iter().flatten().collect();
-
 	let len_x = data_transpose.len();
 	let len_y = data_transpose.get(0).
 		ok_or(miette!("Image has no width lol"))?
 		.len();
+
+	// Flatten data vec
+	let data_flat: Vec<f64> = data_transpose.into_iter().flatten().collect();
 
 	ContourBuilder::new(len_x, len_y)
 		.use_quad_tree(true)
@@ -49,14 +48,14 @@ fn contours(data: Vec<Vec<f64>>) -> Result<Vec<Band>> {
 
 
 /// Builds a topological hierarchy of polygons
-fn hierarchy<'a>(polygons: &'a [&Polygon]) -> Result<ContourHierarchy<'a>> {
+fn hierarchy(polygons: Vec<Polygon>) -> Result<ContourHierarchy> {
 	let mut polygons = polygons.to_vec();
 	polygons.sort_by(|a, b| a.unsigned_area().total_cmp(&b.unsigned_area()));
 	// TODO: ^ cache computed areas during sort?
 	// Can't use sort_by_cached_key because f64 doesn't implement Ord :(
 
 	let mut root = ContourHierarchy {
-		contour: polygons.first().ok_or(miette!("No polygons in contour"))?,
+		contour: polygons.first().ok_or(miette!("No polygons in contour"))?.clone(),
 		children: vec![],
 	};
 
@@ -75,7 +74,7 @@ fn hierarchy<'a>(polygons: &'a [&Polygon]) -> Result<ContourHierarchy<'a>> {
 
 		// Found a leaf node, add the polygon as a child
 		parent.children.push(ContourHierarchy {
-			contour: polygon,
+			contour: polygon.clone(),
 			children: vec![],
 		});
 	}
